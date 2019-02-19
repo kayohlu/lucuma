3b73afb85f1b111eaa3a30ba5ebeea9095a3e1b9
Karl Grogan <krlgrgn@gmail.com>
2019-02-19 20:42:19 +0000

    Change sub menu color. Include fontawesome as npm dependency.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -65,6 +65,16 @@ module.exports = (env, options) => ({
             }
           }
         ]
+      },
+       {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: '../fonts'
+          }
+        }]
       }
     ]
   },
