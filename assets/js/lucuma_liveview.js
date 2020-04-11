cc1762255558773cf3e69b312d7a2ed284259497
Karl Grogan <krlgrgn@gmail.com>
2020-04-11 12:02:52 +0100

    If the CSRF tag exists in head then find it's content and setup liveview.

--- assets/js/lucuma_liveview.js
+++ assets/js/lucuma_liveview.js
@@ -2,27 +2,31 @@ import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 import * as intlTelInput from 'intl-tel-input';
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}});
-window.liveSocket = liveSocket
-let actualSocket = liveSocket.getSocket()
+let csrfTag = document.querySelector("meta[name='csrf-token']")
+
+if (csrfTag) {
+  let csrfToken = csrfTag.getAttribute("content");
+  let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}});
+  window.liveSocket = liveSocket
+  let actualSocket = liveSocket.getSocket()
 
 
-actualSocket.onMessage(function(message) {
-  var input = document.querySelector("#input-phone");
+  actualSocket.onMessage(function(message) {
+    var input = document.querySelector("#input-phone");
 
-  if (input !== null) {
-    var instance = intlTelInput(input, {
-      initialCountry: "IE",
-      nationalMode: false,
-      geoIpLookup: function(success, failure) {
-        $.get("https://ipinfo.io", function() {}, "jsonp").always(function(resp) {
-          var countryCode = (resp && resp.country) ? resp.country : "";
-          success(countryCode);
-        });
-      }
-    });
-  }
-})
+    if (input !== null) {
+      var instance = intlTelInput(input, {
+        initialCountry: "IE",
+        nationalMode: false,
+        geoIpLookup: function(success, failure) {
+          $.get("https://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+            var countryCode = (resp && resp.country) ? resp.country : "";
+            success(countryCode);
+          });
+        }
+      });
+    }
+  })
 
-liveSocket.connect()
+  liveSocket.connect()
+}
